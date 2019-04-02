a50abb7bf8bacb942444ec2f97f1a043bfcacbcd
Austin Marler <austinnmarler@gmail.com>
2019-04-02 14:07:31 -0700

    started code by making snippets for filtering common word list and getting a random word from the new list

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
-    
+    var wordList = commonWords.filter(function(a){
+        return (a.length >= 3);
+    });
+
+    var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
 })
